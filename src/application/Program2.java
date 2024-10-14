a94b8d69aa2d2c8b2ed887b4b5cf0fa1dd4dced9
Lucas Alves <64313623+Lucas7Alves@users.noreply.github.com>
2024-10-14 10:57:00 -0300

    Add FindById Department

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -10,13 +10,20 @@ public class Program2 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+		
 		/*
 		System.out.println("===== TEST 1: INSERT DEPARTMENT =====");
-		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 		Department department = new Department(null, "Music");
 		departmentDao.insert(department);
 		System.out.println("Insert Completed");
-		*/
+		 */
+
+		System.out.println("===== TEST 2: FIND BY ID DEPARTMENT =====");
+		Department dep = departmentDao.findById(5);
+		System.out.println(dep);
+		
+		
 		sc.close();
 
 	}
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -64,14 +64,41 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 
 	@Override
 	public Department findById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		
+		try {
+			st = conn.prepareStatement("SELECT Id, Name FROM department WHERE Id = ?");
+			
+			st.setInt(1, id);
+			
+			rs = st.executeQuery();
+			
+			if (rs.next()) {
+				Department obj = instantiateDepartment(rs);
+				return obj;
+			}
+			return null;
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.CloseStatement(st);
+			DB.CloseResultSet(rs);
+		}
 	}
 
 	@Override
 	public List<Department> findAll() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	
+	private Department instantiateDepartment(ResultSet rs) throws SQLException {
+		Department dep = new Department();
+		dep.setId(rs.getInt("Id"));
+		dep.setName(rs.getString("Name"));
+		return dep;
 	}
 	
 	
